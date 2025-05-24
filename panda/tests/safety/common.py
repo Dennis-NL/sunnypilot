126533324e40b37499dc570c798616b2cf0c3f24
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2025-05-25 01:46:19 +0200

    Update common.py

--- panda/tests/safety/common.py
+++ panda/tests/safety/common.py
@@ -854,8 +854,7 @@ class PandaSafetyTest(PandaSafetyTestBase):
               continue
             if attr.startswith('TestHyundaiCanfd') and current_test.startswith('TestHyundaiCanfd'):
               continue
-            volkswagen_shared = ('TestVolkswagenMqb', 'TestVolkswagenMlb')
-            if attr.startswith(volkswagen_shared) and current_test.startswith(volkswagen_shared):
+            if {attr, current_test}.issubset({'TestVolkswagenMqbStockSafety', 'TestVolkswagenMqbLongSafety', 'TestVolkswagenMlbStockSafety'}):
               continue
 
             # overlapping TX addrs, but they're not actuating messages for either car
