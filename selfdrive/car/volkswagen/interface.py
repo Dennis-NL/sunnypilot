f6c59b56c9dc8b0b60857a4421b683d34e2efbbe
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2025-05-24 22:48:49 +0200

    MLB

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -51,6 +51,19 @@ class CarInterface(CarInterfaceBase):
       # Panda ALLOW_DEBUG firmware required.
       ret.dashcamOnly = True
 
+    elif ret.flags & VolkswagenFlags.MLB:
+      # Set global MLB parameters
+      ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.volkswagenMlb)]
+      ret.enableBsm = 0x30F in fingerprint[0]  # SWA_01
+
+      # TODO: trans message/gear position ID
+      ret.transmissionType = TransmissionType.automatic
+
+      if any(msg in fingerprint[1] for msg in (0x40, 0x86, 0x103)):  # Airbag_01, LWI_01, ESP_03
+        ret.networkLocation = NetworkLocation.gateway
+      else:
+        ret.networkLocation = NetworkLocation.fwdCamera
+
     else:
       # Set global MQB parameters
       ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.volkswagen)]
@@ -73,9 +86,13 @@ class CarInterface(CarInterfaceBase):
 
     # Global lateral tuning defaults, can be overridden per-vehicle
 
-    ret.steerLimitTimer = 0.4
-    if ret.flags & VolkswagenFlags.PQ:
-      ret.steerActuatorDelay = 0.2
+    ret.steerLimitTimer = 1.0
+    if ret.flags & VolkswagenFlags.PQ or ret.flags & VolkswagenFlags.MLB:
+      ret.steerActuatorDelay = 0.13
+      ret.steerRatio = 16.0
+      ret.mass = 1979.
+      ret.wheelbase = 2.8
+      ret.centerToFront = ret.wheelbase * 0.46
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     else:
       ret.steerActuatorDelay = 0.1
@@ -87,7 +104,7 @@ class CarInterface(CarInterfaceBase):
 
     # Global longitudinal tuning defaults, can be overridden per-vehicle
 
-    ret.experimentalLongitudinalAvailable = ret.networkLocation == NetworkLocation.gateway or docs
+    ret.experimentalLongitudinalAvailable = not ret.flags & VolkswagenFlags.MLB and (ret.networkLocation == NetworkLocation.gateway or docs)
     if experimental_long:
       # Proof-of-concept, prep for E2E only. No radar points available. Panda ALLOW_DEBUG firmware required.
       ret.openpilotLongitudinalControl = True
